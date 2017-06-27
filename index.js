52a7a4718e3d32caebf89ecc9337e9203f923c11
xiazhiyu <xiazhiyu2010@126.com>
2017-06-27 21:53:23 +0800

    code

---
+++ index.js
@@ -0,0 +1,14 @@
+import "./images/photo_lighting.jpg";
+import "./images/photo_seating.jpg";
+import "./images/photo_lounge.jpg";
+import "./images/icon_star.svg";
+import "./images/icon_gear.svg";
+import "./images/icon_marker.svg";
+import "./images/logo.svg";
+import "./images/banner_625.jpg";
+import "./images/logo_small.svg";
+import "./images/banner_425.jpg";
+import "./images/banner_825.jpg";
+import "./images/banner_1200.jpg";
+import "./css/reset.css";
+import "./css/screen.css";
